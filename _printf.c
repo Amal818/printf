4f8adee5b2c99266aecbb7ba48f2fcfb6a414a95
Amal7Hope <elmouchiramal@gmail.com>
2023-04-25 19:50:40 -0700

    m

--- _printf.c
+++ _printf.c
@@ -34,7 +34,11 @@ int _printf(const char *format, ...)
 			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list, buffer,flags, width, precision
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list,
+				       	buffer,flags, width, precision
 					, size);
 			if (printed == -1)
 				return (-1);
